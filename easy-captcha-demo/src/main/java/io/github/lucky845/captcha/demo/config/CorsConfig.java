06cf6898ba9842e6826d5c71e5d4172c0deb5938
lucky845 <lc1447545564@gmail.com>
2025-02-26 15:58:11 +0800

    feat: add cors config

---
+++ easy-captcha-demo/src/main/java/io/github/lucky845/captcha/demo/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package io.github.lucky845.captcha.demo.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author created by lucky845 on 2025-02-26
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    /**
+     * 跨域配制
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 所有接口
+                .allowCredentials(true) // 是否发送 Cookie
+                .allowedOriginPatterns("*") // 支持域
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 支持方法
+                .allowedHeaders("*")
+                .exposedHeaders("*");
+    }
+
+
+}
